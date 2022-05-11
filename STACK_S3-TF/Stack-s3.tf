72e91d18cea3aaabb0e2ba56d82f7808084082fa
sin <magicsin@gmail.com>
2022-05-11 19:42:01 -0400

    SC8-751:Delete_Bucket_S3_David_v1.1

--- STACK_S3-TF/Stack-s3.tf
+++ STACK_S3-TF/Stack-s3.tf
@@ -1,10 +1,11 @@
 resource "aws_s3_bucket" "b" {
  bucket = "stackautomationbucketdavid"
 
+ force_destroy = true
+
 }
 
 resource "aws_s3_bucket_acl" "stack_bucket_acl" {
      bucket = aws_s3_bucket.b.id
      acl = "public-read-write"
  }
-##
