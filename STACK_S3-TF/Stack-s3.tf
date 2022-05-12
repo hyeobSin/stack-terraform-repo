8973a0227c547cf2d01c05d74d32278ee814916a
sin <magicsin@gmail.com>
2022-05-11 20:07:39 -0400

    SC8-Static_Website_Hosting_David_v1.6

--- STACK_S3-TF/Stack-s3.tf
+++ STACK_S3-TF/Stack-s3.tf
@@ -58,3 +58,24 @@ resource "aws_s3_bucket" "object_log_bucket" {
   bucket = "object-level-logging-bucket"
   acl    = "log-delivery-write"
 }
+
+resource "aws_s3_bucket_website_configuration" "static_website" {
+  bucket = aws_s3_bucket.static_website.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+  }
+}
